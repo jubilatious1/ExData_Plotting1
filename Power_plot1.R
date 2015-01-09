baa3751c22fef1cf3971170d840564a9f2d57060
jubilatious1 <wjm1@caa.columbia.edu>
2015-01-09 02:13:45 -0800

    Power_plot1 Initial Commit
    Reference source github repo and data source URL

---
+++ Power_plot1.R
@@ -0,0 +1,6 @@
+#Course Project 1
+#Fork Repository from:
+https://github.com/rdpeng/ExData_Plotting1
+
+#Data available at:
+"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
