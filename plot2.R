cff45ba62ba0b0c7e3902e3b79eb5232ccd13e0f
jubilatious1 <wjm1@caa.columbia.edu>
2015-01-11 11:10:47 -0800

    Rename "Power_plots" to plot 1, 2 etc.
    Per homework instructions. Same with R scripts.

---
+++ plot2.R
@@ -0,0 +1,98 @@
+#Course Project 1
+#Fork Github Repository from:
+#https://github.com/rdpeng/ExData_Plotting1
+
+#####Istructions from Assignment web page= https://class.coursera.org/exdata-010/human_grading/view/courses/973504/assessments/3/submissions #######
+
+# When loading the dataset into R, please consider the following:
+#
+# The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much memory the dataset will require in memory before reading into R. Make sure your computer has enough memory (most modern computers should be fine).
+#
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from just those dates rather than reading in the entire dataset and subsetting to those dates.
+#
+# You may find it useful to convert the Date and Time variables to Date/Time classes in R using the strptime() and as.Date() functions.
+#
+# Note that in this dataset missing values are coded as ?.
+
+##########
+
+#Data available at:
+#"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+#go to home directory
+setwd("~/")
+
+#create new directory for download, use abbreviation "HPC" for Household Power Consumption.
+if (!file.exists("HPC_data")) {
+	dir.create("HPC_data")
+}
+
+#move to new directory
+setwd("~/HPC_data")
+
+# Download the data and unzip it
+source <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+library(RCurl)
+
+# create a temporarily file
+file_zipped <- tempfile()
+
+# download file
+download.file(url=source, destfile=file_zipped, method="curl", mode="wb")
+
+# unzip file to the current working directory
+unzip(zipfile=file_zipped, exdir=".")
+
+# clean the temporarily file
+rm(file_zipped)
+
+#check file name
+#dir()
+#list.files()
+#[1] "household_power_consumption.txt"
+
+#read data using read.csv2 command which defaults to ";" separator
+HPC_data <- read.csv2("./household_power_consumption.txt", header= TRUE, col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), dec = ".",  stringsAsFactors = FALSE, na.strings = "?")
+
+#check import
+#head(HPC_data)
+#str(HPC_data)
+#summary(HPC_data)
+
+
+library(lubridate)
+
+HPC_data$Date <- dmy(HPC_data$Date, tz = "America/Los_Angeles")
+
+HPC_data$Time <- hms(HPC_data$Time)
+
+HPC_data$Date <- (HPC_data$Date + HPC_data$Time)
+
+HPC_data <- HPC_data[,-2]
+
+#head(HPC_data)
+#str(HPC_data)
+
+#dtime <- difftime(as.POSIXct("2007-02-03"), as.POSIXct("2007-02-01"),units="mins")
+
+feb_1st_2nd_2007 <- subset(HPC_data, HPC_data$Date >= "2007-02-01 00:00:00" & HPC_data$Date < "2007-02-03 00:00:00")
+
+#head(feb_1st_2nd_2007)
+#str(feb_1st_2nd_2007)
+#summary(feb_1st_2nd_2007)
+
+
+library(zoo)
+
+feb_1st_2nd_2007_zoo <- zoo(feb_1st_2nd_2007[,2:8], feb_1st_2nd_2007$Date)
+
+png(file = "Power_plot2.png", width = 480, height = 480)
+
+#Insert plot code here
+plot(feb_1st_2nd_2007_zoo$Global_active_power, ylab =  "Global Active Power (kilowatts)")
+
+dev.off()
+
+
+
